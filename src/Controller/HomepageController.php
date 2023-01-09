f2a2efd7645d5c60ff420f48b1245b1a8493307b
Bryan Bokma <bryanbokma4@gmail.com>
2023-01-09 14:14:02 +0100

    Meertaligheid af

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Locale;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/')]
 
@@ -24,5 +27,28 @@ class HomepageController extends AbstractController
     public function backhome()
     {
         return $this->redirectToRoute('homepage');
+    }
+
+    // #[Route(path: [
+    //     'en' => 'contact-us',
+    //     'nl' => 'neem-contact-op'
+    // ], name: 'contact')]
+
+    /**
+    * @Route({
+    *     "en": "/contact-us",
+    *     "nl": "/neem-contact-op"
+    * }, name="contact")
+    */
+
+    public function contact(Request $request)
+    {
+        $locale = $request->getLocale();
+        $msg = "This page is in English";
+        if($locale == "nl") 
+        {
+            $msg = "Deze pagina is in het Nederlands";
+        }
+        return new Response("<html><body>$msg</body></html>"); 
     }
 }
