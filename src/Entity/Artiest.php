b3de9d4b42afb4328168dd7b7f85614f0791e2d7
Bryan Bokma <bryanbokma4@gmail.com>
2023-01-10 12:44:47 +0100

    Entities maken af

---
+++ src/Entity/Artiest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArtiestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArtiestRepository::class)]
+class Artiest
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $naam;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $genre;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $omschrijving;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $afbeelding_url;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $website;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNaam(): ?string
+    {
+        return $this->naam;
+    }
+
+    public function setNaam(string $naam): self
+    {
+        $this->naam = $naam;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(string $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getOmschrijving(): ?string
+    {
+        return $this->omschrijving;
+    }
+
+    public function setOmschrijving(string $omschrijving): self
+    {
+        $this->omschrijving = $omschrijving;
+
+        return $this;
+    }
+
+    public function getAfbeeldingUrl(): ?string
+    {
+        return $this->afbeelding_url;
+    }
+
+    public function setAfbeeldingUrl(string $afbeelding_url): self
+    {
+        $this->afbeelding_url = $afbeelding_url;
+
+        return $this;
+    }
+
+    public function getWebsite(): ?string
+    {
+        return $this->website;
+    }
+
+    public function setWebsite(string $website): self
+    {
+        $this->website = $website;
+
+        return $this;
+    }
+}
