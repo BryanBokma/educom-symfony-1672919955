ac93d460778453b17b8f1ebf6974fe1527c2a776
Bryan Bokma <116903663+BryanBokma@users.noreply.github.com>
2023-01-15 23:47:38 +0100

    command geen idee

--- src/Command/CreateArtistCommand.php
+++ src/Command/CreateArtistCommand.php
@@ -13,11 +13,12 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 class CreateArtistCommand extends Command
 {
+    
     protected function configure(): void
     {
         $this
             ->setName("app:artist:create")
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addArgument('artiest', InputArgument::OPTIONAL, 'Argument description')
             ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
@@ -25,7 +26,7 @@ class CreateArtistCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $arg1 = $input->getArgument('artiest');
 
         if ($arg1) {
             $io->note(sprintf('You passed an argument: %s', $arg1));
