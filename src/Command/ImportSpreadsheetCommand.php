dcde1d964566f340b6189f3d81e0c31cb43374ab
Bryan Bokma <bryanbokma4@gmail.com>
2023-01-16 14:26:10 +0100

    command klaar

--- src/Command/ImportSpreadsheetCommand.php
+++ src/Command/ImportSpreadsheetCommand.php
@@ -14,10 +14,10 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-#[AsCommand(
-    name: 'ImportSpreadsheet',
-    description: 'Add a short description for your command',
-)]
+// #[AsCommand(
+//     name: 'ImportSpreadsheet',
+//     description: 'Add a short description for your command',
+// )]
 class ImportSpreadsheetCommand extends Command
 {
     protected function configure(): void
@@ -26,32 +26,39 @@ class ImportSpreadsheetCommand extends Command
             ->setName('app:import-spreadsheet')
             ->setDescription('Import Excel Spreadsheet')
             ->setHelp('This command allows you to import a spreadsheet')
-            ->addArgument('naam', InputArgument::REQUIRED, 'naam')
-            ->addArgument('adres', InputArgument::REQUIRED, 'adres')
-            ->addArgument('postcode', InputArgument::REQUIRED, 'postcode')
-            ->addArgument('woonplaats', InputArgument::REQUIRED, 'woonplaats')
-            ->addArgument('telefoonnummer', InputArgument::REQUIRED, 'telefoonnummer')
-            ->addArgument('email', InputArgument::REQUIRED, 'email')
-            ->addArgument('website', InputArgument::REQUIRED, 'website')
-            ->addArgument('logo_url', InputArgument::REQUIRED, 'logo_url')
-            ->addArgument('afbeelding_url', InputArgument::REQUIRED, 'afbeelding_url')
+            ->addArgument('file', InputArgument::REQUIRED, 'Spreadsheet')
+            // ->addArgument('naam', InputArgument::REQUIRED, 'naam')
+            // ->addArgument('adres', InputArgument::REQUIRED, 'adres')
+            // ->addArgument('postcode', InputArgument::REQUIRED, 'postcode')
+            // ->addArgument('woonplaats', InputArgument::REQUIRED, 'woonplaats')
+            // ->addArgument('telefoonnummer', InputArgument::REQUIRED, 'telefoonnummer')
+            // ->addArgument('email', InputArgument::REQUIRED, 'email')
+            // ->addArgument('website', InputArgument::REQUIRED, 'website')
+            // ->addArgument('logo_url', InputArgument::REQUIRED, 'logo_url')
+            // ->addArgument('afbeelding_url', InputArgument::REQUIRED, 'afbeelding_url')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $data = [
-            "naam" => $input->getArgument('naam'),
-            "adres" => $input->getArgument('adres'),
-            "postcode" => $input->getArgument('postcocde'),
-            "woonplaats" => $input->getArgument('woonplaats'),
-            "telefoonnummer" => $input->getArgument('telefoonnummer'),
-            "email" => $input->getArgument('email'),
-            "website" => $input->getArgument('website'),
-            "logo_url" => $input->getArgument('logo_url'),
-            "afbeelding_url" => $input->getArgument('afbeelding_url')
-        ];
+        // $data = [
+        //     "naam" => $input->getArgument('naam'),
+        //     "adres" => $input->getArgument('adres'),
+        //     "postcode" => $input->getArgument('postcocde'),
+        //     "woonplaats" => $input->getArgument('woonplaats'),
+        //     "telefoonnummer" => $input->getArgument('telefoonnummer'),
+        //     "email" => $input->getArgument('email'),
+        //     "website" => $input->getArgument('website'),
+        //     "logo_url" => $input->getArgument('logo_url'),
+        //     "afbeelding_url" => $input->getArgument('afbeelding_url')
+        // ];
+        
+        $inputFileName = $input->getArgument('file');
         $spreadsheet = IOFactory::load($inputFileName);
+
+        $matrix = $spreadsheet->getSheet(0)->toArray();
+
+        echo var_dump($matrix);
 
         return COMMAND::SUCCESS;
     }
