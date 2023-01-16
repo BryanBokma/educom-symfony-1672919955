970f6a075831f536f1a5b42d4aed35f2b383b46c
Bryan Bokma <bryanbokma4@gmail.com>
2023-01-16 11:35:42 +0100

    Command artiest service af

--- src/Command/CreateArtistCommand.php
+++ src/Command/CreateArtistCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -10,39 +11,44 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+use App\Service\ArtiestService;
+use phpDocumentor\Reflection\Types\Parent_;
 
 class CreateArtistCommand extends Command
 {
-    private $artiest;
-
-    private function fetchArtiest($id)
-    {
-        return $this->artiest->fetchArtiest($id);
+    private $artiestService;
+    
+    public function __construct(ArtiestService $artiestService) {
+        parent::__construct();
+        
+        $this->artiestService = $artiestService;   
     }
     
     protected function configure(): void
     {
         $this
             ->setName("app:artist:create")
-            ->addArgument('artiest', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->addArgument('naam', InputArgument::REQUIRED, 'naam')
+            ->addArgument('genre', InputArgument::REQUIRED, 'genre')
+            ->addArgument('omschrijving', InputArgument::REQUIRED, 'omschrijving')
+            ->addArgument('afbeelding_url', InputArgument::REQUIRED, 'afbeelding_url')
+            ->addArgument('website', InputArgument::REQUIRED, 'website')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('artiest');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        $data = [
+            "naam" => $input->getArgument('naam'),
+            "genre" => $input->getArgument('genre'),
+            "omschrijving" => $input->getArgument('omschrijving'),
+            "afbeelding_url" => $input->getArgument('afbeelding_url'),
+            "website" => $input->getArgument('website')
+        ];
 
-        if ($input->getOption('option1')) {
-            return $this->fetchArtiest($id);
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $this->artiestService->saveArtiest($data);
 
         return Command::SUCCESS;
     }
---
+++ src/Service/ArtiestService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Artiest;
+
+class ArtiestService {
+
+    private $artiestRepository;
+
+    public function __construct(EntityManagerInterface $em) {
+        $this->artiestRepository = $em->getRepository(Artiest::class);
+    }
+
+    public function saveArtiest($params) {
+        $data = [
+          "id" => (isset($params["id"]) && $params["id"] != "") ? $params["id"] : null,
+          "naam" => $params["naam"],
+          "genre" => $params["genre"],
+          "omschrijving" => $params["omschrijving"],
+          "afbeelding_url" => $params["afbeelding_url"],
+          "website" => $params["website"]              
+        ];
+
+        echo var_dump($data);
+
+        $result = $this->artiestRepository->saveArtiest($data);
+        return($result);
+    }
+}
