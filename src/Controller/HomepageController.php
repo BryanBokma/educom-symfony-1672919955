8206ec84e9083a451140ffaab440927ff852e5b3
Bryan Bokma <116903663+BryanBokma@users.noreply.github.com>
2023-01-16 23:42:53 +0100

    json

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -10,6 +10,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
+//Serializer 
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
 use App\Entity\Optreden;
 
 #[Route('/homepage')]
@@ -88,5 +94,15 @@ class HomepageController extends AbstractController
     public function saveData(Request $request) {
         $params = $request->request->all();
         dd($params);
+    }
+
+    #[Route('optredens', name: 'data_optredens')]
+
+    public function ophalenOptredens() 
+    {
+        $rep = $this->getDoctrine()->getRepository(Optreden::class);
+        $data = $rep->getAllOptredens();
+        
+        dump($data);
     }
 }
